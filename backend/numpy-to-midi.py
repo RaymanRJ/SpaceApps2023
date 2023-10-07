ab8337f33a7d433539c715f8f43fda5d8c2f0a48
Rayman Jamal <RaymanRJ@Live.com>
2023-10-07 14:26:38 -0400

    wip

--- backend/numpy-to-midi.py
+++ backend/numpy-to-midi.py
@@ -1,17 +1,49 @@
-def handler(event, context):
-    # Get the body of the request
-    body = json.loads(event['body'])
+import numpy as np
+from mido import MidiFile, MidiTrack, Message
 
-    # Get the numpy array from the body
-    numpy_array = np.array(body['numpy_array'])
 
-    # Convert the numpy array to a MIDI file
-    midi_file = midi.numpy_to_midi(numpy_array)
+pretend_picture = np.random.randint(0, 256, (5, 5, 3), dtype=np.uint8)
+# print(np.array2string(pretend_picture, formatter={'all': lambda x: f'{x: >5}'}))
 
-    # Return the MIDI file as a base64 string
-    return {
-        'statusCode': 200,
-        'body': json.dumps({
-            'midi_file': midi_file
-        })
-    }
+# def play_midi(file_path: str):
+
+def handler(picture: np.ndarray, file_path: str) -> str:
+    # Create a new MIDI file with one track
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
+
+    rows, cols, colors = picture.shape
+
+    r_note = 0
+    g_note = 0
+    b_note = 0
+    for row in range(rows):
+        for col in range(cols):
+            r, g, b = picture[row, col]
+
+            r_note += int(r * 127 / 255)  # Map 0-255 to 0-127
+            g_note += int(g * 127 / 255)  # Map 0-255 to 0-127
+            b_note += int(b * 127 / 255)  # Map 0-255 to 0-127
+
+            print(r_note, g_note, b_note)
+
+            # Add a note-on message
+            track.append(Message('note_on', note=r_note, velocity=64, time=0))
+            track.append(Message('note_on', note=g_note, velocity=64, time=0))
+            track.append(Message('note_on', note=b_note, velocity=64, time=0))
+
+            # Add a note-off message (here, after 500 ticks)
+            track.append(Message('note_off', note=r_note, velocity=64, time=500))
+            track.append(Message('note_off', note=g_note, velocity=64, time=500))
+            track.append(Message('note_off', note=b_note, velocity=64, time=500))
+
+    mid.save(file_path)
+    return file_path
+
+
+
+midi_path = "midi.mid"
+handler(pretend_picture, midi_path)
+
+print(pretend_picture.shape)
