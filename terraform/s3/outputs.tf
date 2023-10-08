bed4f5f10af89d60012c9e472592a27ad07abc5c
Rayman Jamal <34038866+RaymanRJ@users.noreply.github.com>
2023-10-07 20:29:32 -0400

    Debug Midi Production (#10)

--- terraform/s3/outputs.tf
+++ terraform/s3/outputs.tf
@@ -3,7 +3,7 @@ output "bucket_regional_domain_name" {
 }
 
 output "s3_arn" {
-  value = aws_s3_bucket.spaceapps2023_bucket.arn
+  value = aws_s3_bucket.spaceapps2023_bucket_assets.arn
 }
 
 output "s3_asset_arn"{
