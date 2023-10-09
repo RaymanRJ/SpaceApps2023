92c4beaec35e8e65f41c0bf5d8758758b4b58bb8
Rayman Jamal <RaymanRJ@Live.com>
2023-10-08 21:22:56 -0400

    fix deploy

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -68,33 +68,13 @@ const About = () => {
   };
 
   return (
-    <DocumentMeta {...meta}>
-    <div className="main-padding py-28">
-      <div className="md:mx-16 mx-1 bg-white/50 backdrop-blur-md rounded-xl md:px-32 sm:px-14 px-5">
-        <div className="pb-28">
-          <div className="py-20 mx-auto text-center">
-            <h3>Team</h3>
-            <div
-              to="/"
-              className="flex text-white items-baseline justify-center"
-            >
-              <h2 className="h-fit">star</h2>
-              <h1 className="italic text-proj_blue">SCREAM</h1>
-            </div>
-          </div>
-          <p>
-            We are passionate developers with diverse backgrounds excited to take on this challenge. 
-            We created a method of sonifying 3D NASA fly-through photos. 
-            <br/>
-            From teaching English overseas to different engineering professions to software development, 
-            our experiences vary greatly but solving problems is what brings us together.
-          </p>
-        </div>
-
-        <div className="relative pb-[650px]">
-          <h2 className="pb-16 text-center text-proj_blue">Team Members</h2>
-          <div className="flex justify-around gap-8 flex-wrap">
-            {info.map((member, i) => (
+    <div>
+      <DocumentMeta {...meta}>
+      <div className="main-padding py-28">
+        <div className="md:mx-16 mx-1 bg-white/50 backdrop-blur-md rounded-xl md:px-32 sm:px-14 px-5">
+          <div className="pb-28">
+            <div className="py-20 mx-auto text-center">
+              <h3>Team</h3>
               <div
                 to="/"
                 className="flex text-white items-baseline justify-center"
@@ -104,64 +84,62 @@ const About = () => {
               </div>
             </div>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              We are passionate developers with diverse backgrounds excited to take on this challenge. 
+              We created a method of sonifying 3D NASA fly-through photos. 
+              <br/>
+              From teaching English overseas to different engineering professions to software development, 
+              our experiences vary greatly but solving problems is what brings us together.
             </p>
           </div>
-
-          <div className="relative pb-[650px]">
-            <h2 className="pb-16 text-center text-proj_blue">Team Members</h2>
-            <div className="flex justify-around gap-8 flex-wrap">
-              {info.map((member, i) => (
-                <div
-                  key={i}
-                  className="group cursor-pointer member"
-                  onClick={() => {
-                    setMember(i);
-                    scroll();
-                  }}
-                >
-                  <img
-                    src={member.pic}
-                    alt={member.name}
-                    className="rounded-xl w-64 h-72 object-cover group-hover:scale-110 duration-300"
-                  />
-                  <h4 className="w-64 text-center py-6">{member.name}</h4>
-                </div>
-              ))}
-            </div>
-
-            <div className="mt-10 desc-win">
-              <div className="generic flex flex-col justify-center">
-                <h4 className="md:w-1/3 sm:w-full text-center mx-auto">
-                  Click on a member of the team to learn about them
-                </h4>
+            <div className="relative pb-[650px]">
+              <h2 className="pb-16 text-center text-proj_blue">Team Members</h2>
+              <div className="flex justify-around gap-8 flex-wrap">
+                {info.map((member, i) => (
+                  <div
+                    key={i}
+                    className="group cursor-pointer member"
+                    onClick={() => {
+                      setMember(i);
+                      scroll();
+                    }}
+                  >
+                    <img
+                      src={member.pic}
+                      alt={member.name}
+                      className="rounded-xl w-64 h-72 object-cover group-hover:scale-110 duration-300"
+                    />
+                    <h4 className="w-64 text-center py-6">{member.name}</h4>
+                  </div>
+                ))}
               </div>
 
-              {info.map((info, i) => (
-                <div key={i} className="info">
-                  <h2>{info.name}</h2>
-                  <div>
-                    <h4 className="pb-3">Background:</h4>
-                    <p>{info.background}</p>
-                  </div>
-                  <div>
-                    <h4 className="pb-3">Project Role:</h4>
-                    <p>{` - ${info.role[0]} - `}</p>
-                    <p>{` - ${info.role[1]} - `}</p>
-                  </div>
+              <div className="mt-10 desc-win">
+                <div className="generic flex flex-col justify-center">
+                  <h4 className="md:w-1/3 sm:w-full text-center mx-auto">
+                    Click on a member of the team to learn about them
+                  </h4>
                 </div>
-              ))}
+
+                {info.map((info, i) => (
+                  <div key={i} className="info">
+                    <h2>{info.name}</h2>
+                    <div>
+                      <h4 className="pb-3">Background:</h4>
+                      <p>{info.background}</p>
+                    </div>
+                    <div>
+                      <h4 className="pb-3">Project Role:</h4>
+                      <p>{` - ${info.role[0]} - `}</p>
+                      <p>{` - ${info.role[1]} - `}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </DocumentMeta>
+      </DocumentMeta>
+    </div>
   );
 };
 
