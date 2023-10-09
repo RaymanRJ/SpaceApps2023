0ceceec8fa591bdf9250b04827f0efb0c968f598
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2023-10-08 21:10:37 -0400

    Update About.js (#27)

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -81,13 +81,11 @@ const About = () => {
             </div>
           </div>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            We are passionate developers with diverse backgrounds excited to take on this challenge. 
+            We created a method of sonifying 3D NASA fly-through photos. 
+            <br/>
+            From teaching English overseas to different engineering professions to software development, 
+            our experiences vary greatly but solving problems is what brings us together.
           </p>
         </div>
 
